72e37bb85eb7f8b07e5acfb4fd9bb7ceb30f7e0c
Andrew Newman <deadloko@gmail.com>
2019-11-17 12:10:22 +0300

    clazy: init at 1.6

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9918,6 +9918,8 @@ in
 
   chruby = callPackage ../development/tools/misc/chruby { rubies = null; };
 
+  clazy = callPackage ../development/tools/clazy { };
+
   cl-launch = callPackage ../development/tools/misc/cl-launch {};
 
   cloud-nuke = callPackage ../development/tools/cloud-nuke { };
